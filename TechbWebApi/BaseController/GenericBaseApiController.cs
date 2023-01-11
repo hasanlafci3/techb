69fc44dc3dfd21ab996ccf4d505d4c2df22ccab9
Recep <recep.yesil@payolog.com>
2023-01-11 09:46:05 +0300

    new controllers added

---
+++ TechbWebApi/BaseController/GenericBaseApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace TechbWebApi.BaseController
+{
+    [Route("api/tech/aim/[controller]/[action]")]
+    [ApiController]
+    public class GenericBaseApiController : ControllerBase
+    {
+        [HttpGet]
+        public string HeartBeat()
+        {
+            return DateTime.UtcNow.ToLongDateString();
+        }
+    }
+}
---
+++ TechbWebApi/BaseController/GenericBaseApiTestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechbWebApi.BaseController
+{
+
+    public class GenericBaseApiTestController : GenericBaseApiController
+    {
+        public GenericBaseApiTestController()
+        {
+           
+        }
+
+
+        [HttpGet]
+        public string Get(string id)
+        {
+            return $"Generic  {id}";
+        }
+    }
+}
