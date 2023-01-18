8841a1d8f2be92ab020e5690e353526a64120ce3
Recep <recep.yesil@payolog.com>
2023-01-18 10:16:48 +0300

    add new project

---
+++ TechaApiIdentityMvc/TechaApiIdentityMvc/Models/ApplicationUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TechaApiIdentityMvc.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Display(Name = "FirstName and LastName")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Display(Name = "Gender")]
+        public string Gender { get; set; }
+
+        [Display(Name = "Date Of Birth")]
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
+
+
+
